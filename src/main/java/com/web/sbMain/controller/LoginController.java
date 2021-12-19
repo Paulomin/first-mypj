4f3aaefad0bcf351de553ab6fc78baf4bebde3a1
User <User@DESKTOP-9GGNQC3>
2021-12-19 15:51:31 +0900

    로그인 창 파일 생성

---
+++ src/main/java/com/web/sbMain/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.web.sbMain.controller;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String view = "/WEB-INF/jsp/login/login.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(view);
+		rd.forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/login/login.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>OSC Login</title>
+	<jsp:include page="/WEB-INF/jsp/head/defalut.jsp" flush="false" />
+</head>
+<body>
+		<jsp:include page="/WEB-INF/jsp/head/top-menu.jsp" flush="false" /> 
+	
+</body>
+</html>

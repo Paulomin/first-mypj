c8c2d0c7ccfea6bc2f8447568286e3ba02527fa7
JaeWanJang <newmy526@naver.com>
2021-12-22 20:41:05 +0900

    회원가입 모델 작업중

--- src/main/java/com/web/sbMain/model/SignService.java
+++ src/main/java/com/web/sbMain/model/SignService.java
@@ -1,10 +1,28 @@
 package com.web.sbMain.model;
 
-public class SignService {
+import java.util.*;
 
+public class SignService {
+	
+	private boolean isEmpty(String str) {
+		// 빈칸인 경우
+		return str.isEmpty();
+	}
+	
 	public boolean login(SignDTO dto) {
-		// TODO Auto-generated method stub
-		return false;
+		SignDAO dao = new SignDAO();
+		List<SignDTO> data = dao.select(dto.getUserid());
+		if(data.size() == 1) {
+			SignDTO signUserData = data.get(0);
+			if(dto.equalsPassword(signUserData)) {
+				dto.setUserid(signUserData.getUserid());
+				dto.setPassword(signUserData.getPassword());
+				dto.setUsername(signUserData.getUsername());
+				dto.setEmail(signUserData.getEmail());
+				dto.setPhone(signUserData.getPhone());
+				dao.close();
+			}
+		}
 	}
 
 }

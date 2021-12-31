a13b9a50af24247eb8b163eb9ccfd73c67f0f765
JaeWanJang <newmy526@naver.com>
2021-12-31 18:06:59 +0900

    회원가입 중복검사 로직 짜는 중

--- src/main/webapp/static/js/checkJoin.js
+++ src/main/webapp/static/js/checkJoin.js
@@ -1,6 +1,10 @@
 function userIdCheck() {
 	var userid = document.getElementById('user_id');
-	$.ajax({
+	var msg = document.getElementById('id_check');
+   var ckid = /^[a-z0-9_-]{4,20}$/;
+   var redCss = 'font-size: 13px; color:red; margin-left: 5px;';
+	
+   $.ajax({
 		url:"./sign/check",
 		type: "GET",
 		data: {
@@ -8,11 +12,18 @@ function userIdCheck() {
 		},
 		dataType: "json",
 		success: function(data) {
-			if(data.able === "possible"){
-				alert('사용 가능 합니다.');
-			}else{
-				alert('이 아이디는 사용이 불가능 합니다');
-			}
-		}
-	});
+			if(data.able === "possible" && ckid.test(userid)){
+				msg.innerText = "사용 가능한 아이디 입니다.";
+				msg.style.cssText = 'font-size: 13px; margin-left: 5px;';
+           $('#user_id').focusout(function() {
+              msg.innerText = "";
+           });
+			} else if(!ckid.test(userid)){
+            msg.innerText = "최소 4 ~ 20자를 입력해주세요.";
+            msg.style.cssText = 'font-size: 13px; color:red; margin-left: 5px;';
+         } else if(data.able === "used" && ckid.test(userid)) {
+            msg.innerText = "이미 사용중인 아이디 입니다.";
+            msg.style.cssText = 'font-size: 13px; color:red; margin-left: 5px;';
+         }
+	}});
 }
